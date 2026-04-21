f9edb397f07a91143ca50b413410068360ea1aaf
Daniel <schillingdaniel208@gmail.com>
2026-04-21 12:26:02 +0200

    Added Aufgabe3

--- PÜ2/PÜ2_Aufgabe3/PÜ2_Aufgabe3/src/PÜ2_Aufgabe3.java
+++ PÜ2/PÜ2_Aufgabe3/PÜ2_Aufgabe3/src/PÜ2_Aufgabe3.java
@@ -1,5 +1,31 @@
+import java.util.Scanner;
+
 public class PÜ2_Aufgabe3 {
     public static void main(String[] args)  {
-        System.out.println("Hello, World!");
+        //Eingabe der Tastatur
+		Scanner in = new Scanner(System.in);
+		
+		int i;
+		int j;
+		
+		
+		System.out.println("Eingabe der maximalen Zahlenwerte dessen Primzahlen bestimmt werden sollen:");
+		int n = in.nextInt();
+		
+		boolean[] istPrimzahl = new boolean[n + 1];
+		//Äußere Schleife
+		for(i=2; i + i <= n; i = i + 1) {
+			//Innere Schleife
+			for(j = i+i; j <= n; j = j + i) {
+			    istPrimzahl[j] = true;
+			}
+		}
+		
+		for(i=2; i<=n; i++) {
+			if(istPrimzahl[i] == false)
+				System.out.println(i);
+        }
+
+        in.close();
     }
 }

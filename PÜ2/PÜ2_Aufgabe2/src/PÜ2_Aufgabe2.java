30de37138500a227aa3e3316be54fdb96d5952c6
DanielSchilling <schillingdaniel208@gmail.com>
2026-04-21 13:37:55 +0200

    Added Comentary to Aufgabe2

--- PÜ2/PÜ2_Aufgabe2/src/PÜ2_Aufgabe2.java
+++ PÜ2/PÜ2_Aufgabe2/src/PÜ2_Aufgabe2.java
@@ -1,27 +1,37 @@
 import java.util.Scanner;
 
 public class PÜ2_Aufgabe2 {
-
 	public static void main(String[] args) {
+		
+		//definition von Variablen
 		String input;
-
 		Scanner in = new Scanner(System.in);
 		String newString = "";
+
+		//Eingabeaufforderung
 		System.out.print("Geben Sie eine Zeichenkette ein: ");
 		input = in.nextLine();
-
 		char [] charArray = new char[input.length()];
+
+		//Umrechnung der Zeichenkette
 		for (int i = 0; i < input.length(); i++) {
+
+			//Auslesen des Zeichen an der aktuellen Stelle
 			charArray[i] = input.charAt(i);
+
+			//Abändern des Zeichens bei gegebenen Bedingungen
 			if(charArray[i] > 64 && charArray[i] < 90 || charArray[i] > 96 && charArray[i] < 122) {
 				charArray[i]++;
 			}
 			 else if (charArray[i] == 90 || charArray[i] == 122) {
 				charArray[i] -= 25;
 			}
+			
+			//Hinzufügen des neuen Zeichens zur neuen Zeichenkette
 			newString += charArray[i];
 		}
 
+		//Ausgabe der neuen Zeichenkette
 		System.out.println("Die neue Zeichenkette lautet: " + newString);
 
 		in.close();

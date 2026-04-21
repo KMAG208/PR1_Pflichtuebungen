5e9342b0791cb36777003796d568826d6154bb02
DanielSchilling <schillingdaniel208@gmail.com>
2026-04-21 13:29:19 +0200

    Added comentary to Aufgabe1

--- PÜ2/PÜ2_Aufgabe1/src/Aufgabe1.java
+++ PÜ2/PÜ2_Aufgabe1/src/Aufgabe1.java
@@ -1,7 +1,6 @@
 import java.util.Scanner;
 
 public class Aufgabe1 {
-
 	public static void main(String[] args) {
 		
 		//definition von Variablen
@@ -11,33 +10,39 @@ public class Aufgabe1 {
 		
 		System.out.println("Dieses Programm gibt alle Primzahlen kleiner gleich der eingegeben Zahl aus, \n Wird beendet durch eingabe einer Zahl kleiner als 1");
 		
+		//Berechnungsschleife
 		do {
+
+			//Eingabeaufforderung
 			System.out.println("Bitte geben sie ihre Zahl ein:");
 			eingabe = in.nextInt();
 			
+			//Berechnung der Primzahlen
 			for(int i = 2; i <= eingabe; i++) {
 				for(int j = 2; j< i && isPrim; j++) {
 					if(i%j == 0) {
 						isPrim = false;
 					}
-					
 				}
-				
+
+				//Ausgabe der Primzahl
 				if(isPrim) {
 					System.out.println(i);
 				}
-				
+
+				//Zurücksetzen der Variable für die nächste Zahl
 				isPrim = true;
 			}
-				
+			
+			//Abschlussmeldung
 			System.out.println("Dies waren alle Primzahlen <="+ eingabe);
 			
-			
+		//Schleifenbedingung	
 		}while(eingabe > 1);
 		
+		//Abschlussmeldung
 		System.out.println("... und tschüss");
 		in.close();
 
 	}
-
 }

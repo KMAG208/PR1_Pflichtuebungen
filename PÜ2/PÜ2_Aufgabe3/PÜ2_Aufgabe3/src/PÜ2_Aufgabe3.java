94432a2f12fa4b32d8b99008b9a591be9259e021
DanielSchilling <schillingdaniel208@gmail.com>
2026-04-27 14:19:04 +0200

    Benutererklärung hinzugefügt

--- PÜ2/PÜ2_Aufgabe3/PÜ2_Aufgabe3/src/PÜ2_Aufgabe3.java
+++ PÜ2/PÜ2_Aufgabe3/PÜ2_Aufgabe3/src/PÜ2_Aufgabe3.java
@@ -8,17 +8,19 @@ public class PÜ2_Aufgabe3 {
 		int i;
 		int j;
 		
+		//Erklärung des Programms
+		System.out.println("Dieses Programm gibt alle Primzahlen kleiner gleich der eingegeben Zahl aus, \n Primzahlen können nur von Zahlen >= 2 berechnet werden");
 		//Eingabeaufforderung
 		System.out.println("Eingabe der maximalen Zahlenwerte dessen Primzahlen bestimmt werden sollen:");
 		int n = in.nextInt();
 		boolean[] Markierungen = new boolean[n + 1];
 
 		//Berechnung der Primzahlen
-		//wenn das doppelte von i > als die Eingabe endet die Schleife
+		//Wenn das doppelte von i > als die Eingabe endet die Schleife
 		for(i=2; i + i <= n; i = i + 1) {
 			//j wird immer das doppelte von i gesetzt damit nur die Vielfachen durchlaufen werden
 			for(j = i+i; j <= n; j = j + i) {
-				//speichern der Markierungen an den Stellen an welchen keine Primzahlen sind
+				//Speichern der Markierungen an den Stellen an welchen keine Primzahlen sind
 			    Markierungen[j] = true;
 			}
 		}

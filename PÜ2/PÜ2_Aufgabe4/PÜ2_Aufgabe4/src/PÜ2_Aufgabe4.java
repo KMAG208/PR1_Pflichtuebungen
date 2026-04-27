474e551cfa8cd7e5a077e3830bf39c1fd1eddcf1
DanielSchilling <schillingdaniel208@gmail.com>
2026-04-27 14:32:00 +0200

    Changed comentary in Aufgabe4

--- PÜ2/PÜ2_Aufgabe4/PÜ2_Aufgabe4/src/PÜ2_Aufgabe4.java
+++ PÜ2/PÜ2_Aufgabe4/PÜ2_Aufgabe4/src/PÜ2_Aufgabe4.java
@@ -3,7 +3,7 @@ import java.util.Scanner;
 public class PÜ2_Aufgabe4 {
     public static void main(String[] args) {
 
-        //definition von Variablen
+        //Definition von Variablen
         Scanner in = new Scanner(System.in);
         int uebrig = 0;
         String ergebnis = "";
@@ -12,21 +12,23 @@ public class PÜ2_Aufgabe4 {
         int[] werte = {1000, 900, 500, 400, 100, 90, 50, 40, 10, 9, 5, 4, 1};
         String[] symbole = {"M", "CM", "D", "CD", "C", "XC", "L", "XL", "X", "IX", "V", "IV", "I"};        
 
-        //Eingabeaufforderung
+        //Erklärung des Programms
+        System.out.println("Dieses Programm konvertiert Dezimalzahlen in Römischezahlen \n Die Maximale Zahl ist 3999 und die Minimale Zahl ist 1.");
         System.out.println("Die Maximale Zahl ist 3999 und die Minimale Zahl ist 1.");
-        System.out.print("Geben Sie eine Zahl ein: ");
 
-        //Eingabe der Zahl und Überprüfung der Gültigkeit
+        //Eingabeaufforderung
+        System.out.println("Geben Sie eine Zahl ein: ");
+        
+        //Auslesen der Eingabe und Überprüfung der Gültigkeit
         do {
-            System.out.println("Geben Sie eine Zahl ein: ");
             uebrig = in.nextInt();
+            
             if(uebrig < 1 || uebrig > 3999){
                 System.out.println("Die Zahl ist ungültig. Bitte geben Sie eine Zahl zwischen 1 und 3999 ein: ");
             }
-
         } while(uebrig < 1 || uebrig > 3999);
 
-        //Berechnung der römischen Zahl
+        //Berechnung der römischen Zahl. Berechnung startet mit dem höchsten Wert und arbeitet sich vor zum kleinsten Wert
         for (int i = 0; i < werte.length; i++) {
             //Solange die übrig gebliebene Zahl größer oder gleich dem aktuellen Wert ist
             while (uebrig >= werte[i]) {

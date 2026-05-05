7684554eb846160482a61ef730ea821a06fed2f8
DanielSchilling <schillingdaniel208@gmail.com>
2026-05-05 19:10:56 +0200

    Snackautomat bis getPreis

--- PÜ3/src/Snackautomat.java
+++ PÜ3/src/Snackautomat.java
@@ -2,6 +2,7 @@ import java.util.Scanner;
 
 public class Snackautomat {
     public static void main(String[] args) throws Exception {
+        
         Scanner in = new Scanner(System.in);
         String[][] produkte = {
             {"Wasser", "Cola", "Saft", "Kaffee"},
@@ -15,14 +16,29 @@ public class Snackautomat {
             {85, 92, 111, 95, 89}
         };
 
+        int eProdukt;
+        String eKategorie;
+
+        zeigeProdukte(produkte);
+
+        System.out.println("Geben Sie die Kategorie ein (G, S, O): ");
+        eKategorie = in.nextLine();
+
+        System.out.println("Geben Sie das Produkt ein: ");
+        eProdukt = in.nextInt();
+
+        System.out.printf("Preis: %.2f €%n", getPreis(preise, eKategorie, eProdukt) / 100.0);
+
+    }
+
+
+    /**
+     * Funktion, die die Kategorien des Snackautomaten zurückgibt.
+     * @return Ein Array von Strings, das die Kategorien des Snackautomaten enthält.
+     */
+    public static String[] getKategorien(){
         String[] kategorien = {"G - Getränke", "S - Snacks", "O - Obst"};
-
-        String eProdukt;
-        String ePreis;
-
-        zeigeProdukte(produkte, kategorien);
-
-
+        return kategorien;
     }
 
     /**
@@ -32,13 +48,30 @@ public class Snackautomat {
      * Jedes Produkt wird mit seiner passenden Nummer angezeigt.
      * @param produkte Array von Arrays mit Produktnamen
      */
-    public static void zeigeProdukte(String[][] produkte, String[] kategorien){
-        for(int i = 0; i < kategorien.length; i++){
-            System.out.println(kategorien[i]+ " [ ");
+    public static void zeigeProdukte(String[][] produkte){
+        for(int i = 0; i < getKategorien().length; i++){
+
+            System.out.print(getKategorien()[i]+ " [ ");
             for(int j = 0; j < produkte[i].length; j++){
-                System.out.print(j+ " : "+ produkte[i][j]);
+                System.out.print(j+1+ ": "+ produkte[i][j]+ " ");
             }
-            System.out.print("]");
+            System.out.println("]");
         }
     }
+
+    /**
+ * Gibt den Preis eines ausgewählten Produkts zurück.
+ * @param preise Array von Arrays mit Preisen in Cent
+ * @param kategorie Auswahl der Kategorie als String ("G", "S", "O")
+ * @param produkt Auswahl des Produkts innerhalb der gewählten Kategorie
+ * @return Preis in Cent
+ */
+public static int getPreis(int[][] preise, String kategorie, int produkt) {
+     switch (kategorie) {
+        case "G": return preise[0][produkt - 1]; 
+        case "S" : return preise[1][produkt - 1];
+        case "O" : return preise[2][produkt - 1];
+        default: return 0;
+    }
+}
 }

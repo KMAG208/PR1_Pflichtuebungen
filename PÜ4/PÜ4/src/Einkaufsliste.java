cdb2bdecae217eba65dcbab1d8f5f767adb7b701
KMAG208 <schillingdaniel208@gmail.com>
2026-05-12 11:55:58 +0200

    Created PÜ4

---
+++ PÜ4/PÜ4/src/Einkaufsliste.java
@@ -0,0 +1,5 @@
+public class Einkaufsliste {
+    public static void main(String[] args) throws Exception {
+        System.out.println("Hello, World!");
+    }
+}
---
+++ PÜ4/PÜ4/src/Produkt.java
@@ -0,0 +1,6 @@
+public class Produkt {
+        public static void main(String[] args) throws Exception {
+        System.out.println("Hello, World!");
+    }
+
+}

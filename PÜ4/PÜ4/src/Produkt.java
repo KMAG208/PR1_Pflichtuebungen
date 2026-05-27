26b84b4b3c1795bdff484e9118ac36927b158fb0
Jan1608 <wagenknecht.jan06@gmail.com>
2026-05-27 22:15:48 +0200

    Version 27.05.26

--- PÜ4/PÜ4/src/Produkt.java
+++ PÜ4/PÜ4/src/Produkt.java
@@ -1,13 +1,24 @@
 public class Produkt {
-	String name = "Unknown";
+	 // Name des Produkts, Standardwert "Unknown" falls kein Name angegeben
+	String name = "Unknown"; 
+	
+	// Preis des Produkts, Standardwert 0.0 falls kein Preis angegeben
 	double preis = 0.0;
+	
+	// Zeiger auf das nächste Produkt in der verketteten Liste, null = kein Nachfolger
 	Produkt next = null;
 	
+	
+	// Konstruktor, setzt Name und Preis des Produkts
 	Produkt(String eName, double ePreis){
+		// Prüft ob der übergebene Name gültig ist (nicht null)
 		if(eName != null) {
+			// Setzt den Namen auf den übergebenen Wert
 			name = eName;
 		}
+		// Prüft ob der übergebene Preis gültig ist (nicht negativ)
 		if(ePreis >= 0) {
+			// Setzt den Preis auf den übergebenen Wert
 			preis = ePreis;
 		}
 	}
